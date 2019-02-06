ba5a87f0b75e61851611c45c3be0e7d290e21a54
Unknown <fclaudia.93@gmail.com>
2019-02-05 22:16:53 -0400

    Styles in chat

--- views/Chat.js
+++ views/Chat.js
@@ -10,13 +10,16 @@ export default class Home extends React.Component {
       user: "",
       room: 0,
       msg: "",
-      users: []
+      users: [],
+      msgs: []
     };
-    this.socket = SocketIOClient("http://192.168.1.110:3001");
+    this.socket = SocketIOClient("http://192.168.0.111:3001");
   }
 
-  static navigationOptions = {
-    title: `Chat` // ${this.state.room}`
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: `Chat ${navigation.getParam('room', '').toString()}`,
+    };
   };
 
   componentDidMount = async () => {
@@ -36,13 +39,9 @@ export default class Home extends React.Component {
 
     this.socket.on("message", data => {
       console.log(data);
+      this.setState({ msgs: [...this.state.msgs, data] })
     });
   };
-
-  componentWillUnmount() {
-    console.log("bye");
-    this.socket.disconnect();
-  }
 
   sendMsg() {
     this.socket.emit("send-msg", { text: this.state.msg });
@@ -50,13 +49,23 @@ export default class Home extends React.Component {
   }
 
   render() {
-    let users = [];
+    let users = '';
+    let msgs = []
     for (let i = 0; i < this.state.users.length; i++) {
-      users.push(<Text key={i}>{this.state.users[i]}</Text>);
+      users += ` ${this.state.users[i]}`
+      if (i != this.state.users.length - 1) {
+        users += `,`
+      }
     }
+
     return (
       <View>
-        {users}
+        <Text style={styles.users}>In chat: {users}</Text>
+        {
+          this.state.msgs.map((el, index) => (
+            <Text key={index}><Text style={[el.from == this.state.user ? styles.own : styles.other]}>{el.from}: </Text><Text>{el.text}</Text> <Text style={styles.date}>{el.created}</Text></Text>
+          ))
+        }
 
         <TextInput
           style={styles.input}
@@ -72,12 +81,23 @@ export default class Home extends React.Component {
             }}
           />
         </View>
+        <Button
+          title="Go Back"
+          onPress={() => {
+            console.log("bye");
+            this.socket.disconnect();
+            this.props.navigation.goBack();
+          }}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  users: {
+    marginBottom: 20,
+  },
   input: {
     marginTop: 20,
     marginBottom: 20,
@@ -86,5 +106,13 @@ const styles = StyleSheet.create({
   button: {
     marginBottom: 20,
     marginTop: 20
+  }, own: {
+    color: 'red'
+  }, other: {
+    color: 'blue'
+  },
+  date: {
+    fontSize: 12,
+    color: '#c8c8c8'
   }
 });
